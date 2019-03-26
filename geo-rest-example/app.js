bb0fd640131d4c522b44e9cf77a4bb94a18cd7cc
Thomas-Rosenkrans-Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2019-03-26 12:39:06 +0100

    added message on mongo connection

--- geo-rest-example/app.js
+++ geo-rest-example/app.js
@@ -3,7 +3,15 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./config')
 
-mongoose.connect(config.MONGO_URL, { createIndexes: true, useNewUrlParser: true });
+mongoose.connect(config.MONGO_URL, { createIndexes: true, useNewUrlParser: true }, (err) => {
+    if(err){
+        console.error("Could not connect to MongoDB:")
+        console.error(err)
+        return
+    }
+
+    console.log("Successfully connected to MongoDB.")
+});
 
 const app = express()
 
