d9d3d1b0e2e1b562adacbade1513b2bb1f013c20
Thomas-Rosenkrans-Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2019-06-11 16:27:43 +0200

    fixed rest-api-test example

--- rest-api-test-example/src/JsonFileDatabase.js
+++ rest-api-test-example/src/JsonFileDatabase.js
@@ -74,4 +74,4 @@ class JsonFileDatabase {
     }
 }
 
-module.exports = Jsonentity
+module.exports = JsonFileDatabase
--- rest-api-test-example/src/server.js
+++ rest-api-test-example/src/server.js
@@ -1,9 +1,12 @@
 const createApplication = require('./app')
 const port = 3003
 
-
 const JsonFileDatabase = require('./JsonFileDatabase')
 const people = new JsonFileDatabase('people.json')
-app = createApplication(people)
-app.listen(port)
-console.log("Server started on port " + port)
+const app = createApplication(people)
+app.listen(port, (err) => {
+    if(err)
+        console.error(err)
+    else
+        console.log(`Server started on port ${port}`)
+})
