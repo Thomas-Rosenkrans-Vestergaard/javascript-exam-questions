14f4f8b6a5b81842401c7891f42297ce5ed47046
Thomas-Rosenkrans-Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2019-06-12 19:28:36 +0200

    added skeleton for MongoDatabase

---
+++ rest-api-test-example/src/MongoDatabase.js
@@ -0,0 +1,27 @@
+module.exports = class MongoDatabase {
+
+
+    async all() {
+    
+    }
+
+    async clear() {
+
+    }
+
+    async get(id) {
+
+    }
+
+    async add(entity) {
+
+    }
+
+    async put(id, entity) {
+
+    }
+
+    async delete(idToDelete) {
+
+    }
+}
